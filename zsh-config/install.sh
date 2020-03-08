61be1589f3ecd78134117516dac7593f9edce633
Jonathan Scanzi <jonathan.scanzi.95@gmail.com>
2020-03-08 17:45:48 +0100

    Add check for zsh presence before installing p10k

--- zsh-config/install.sh
+++ zsh-config/install.sh
@@ -1,3 +1,8 @@
+if [ "$(command -v zsh)" == "" ]; then
+        echo 'Error: could not find zsh in your PATH. Please install it and try this script again.'
+        exit 1
+fi
+
 rm -rf ~/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 cp ./.zshrc  ~/.zshrc
