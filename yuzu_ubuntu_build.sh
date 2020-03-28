03c1bbd77a53ff0cc56df6d0f4e8277d33feccbf
Jonathan Scanzi <kratosjon@gmail.com>
2020-03-28 01:17:09 +0100

    Update yuzu_ubuntu_build.sh

--- yuzu_ubuntu_build.sh
+++ yuzu_ubuntu_build.sh
@@ -1,4 +1,4 @@
 rm -rf yuzu-mainline
 sudo apt install -y libsdl2-2.0-0 libsdl2-dev qtbase5-dev libqt5opengl5-dev build-essential libboost-all-dev cmake python2 clang libc++-dev
 git clone --recursive https://github.com/yuzu-emu/yuzu-mainline
-cd yuzu-mainline && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j16
+cd yuzu-mainline && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release && make -j16 && sudo make install
